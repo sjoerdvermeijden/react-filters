96f2dbda2c54e59f1b32d07a1ba48c7f7d15b680
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2022-07-15 16:44:02 +0200

    Initial commit

---
+++ data.js
@@ -0,0 +1,33 @@
+export const data = [
+    {
+        id: 0,
+        name: 'test',
+        utililties: [
+            {
+                pool: true,
+                wifi: true,
+                computer: true
+            }
+        ]
+    },
+    {
+        id: 1,
+        name: 'test1',
+        utililties: [
+            {
+                breakfast: true,
+                computer: true
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: 'test2',
+        utililties: [
+            {
+                pool: true,
+                computer: true
+            }
+        ]
+    }
+]
